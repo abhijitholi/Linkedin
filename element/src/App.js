63a23f2d37117f5c4973caccc57f6562a9646708
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2023-12-26 15:05:23 +0530

    Add files via upload

---
+++ element/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Element from './Element';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <Element/>
+    </div>
+  );
+}
+
+export default App;
---
+++ element/src/Element.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function   Element() {
+  return <p>This is a simple React element!</p>;
+}
+
+export default Element;
